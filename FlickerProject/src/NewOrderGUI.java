bb62d8f9e2fec8c7d7a821bf6577056343c1626f
amiaogao13 <amiaogao13@OlinLab204-18.augustana.edu>
2015-09-11 15:06:54 -0500

    I changed the appearence of the window, so e.g. the "name" is not a label anymore, it's actually in the text field.

--- FlickerProject/src/NewOrderGUI.java
+++ FlickerProject/src/NewOrderGUI.java
@@ -5,43 +5,44 @@ import javax.swing.*;
 
 public class NewOrderGUI {
 
-	private JLabel enter;
-	private JLabel name;
-	private JLabel address;
-	private JLabel phoneNum;
+	
 	private JButton save;
 	private JTextField nameText;
 	private JTextField addressText;
 	private JTextField phoneNumText;
 
-	public void NewOrder() {
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		enter = new JLabel("Enter: ");
-		name = new JLabel("Name: ");
-		address = new JLabel("Address: ");
-		phoneNum = new JLabel("phoneNum: ");
-		save = new JButton("Save");
-		nameText = new JTextField();
-		addressText = new JTextField();
-		phoneNumText = new JTextField();
-		
-		
-		
-		// overall frame
-		frame.setLayout(new GridLayout(4, 2));
-		frame.add(enter);
-		frame.add()
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(new Dimension(400, 300));
-		frame.setTitle("New Order");
-		frame.setVisible(true);
-
-	}
-
 	public static void main(String[] args) {
 		NewOrderGUI gui = new NewOrderGUI();
 
 	}
+	public NewOrderGUI() {
+		
+		save = new JButton("Save");
+		nameText = new JTextField("Name: ");
+		addressText = new JTextField("Address: ");
+		phoneNumText = new JTextField("Phone Number: ");
+		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400,300);
+		frame.setTitle("New Order");
+		frame.setLayout(new BorderLayout());
+		JPanel north = new JPanel();
+		north.add(new JLabel("Enter: "));
+		frame.add(north,BorderLayout.NORTH);
+		JPanel center = new JPanel(new GridLayout(3,1));
+		center.add(nameText);
+		center.add(addressText);
+		center.add(phoneNumText);
+		frame.add(center,BorderLayout.CENTER);
+		JPanel south = new JPanel(new FlowLayout());
+		south.add(save);
+		frame.add(south, BorderLayout.SOUTH);
+		frame.setVisible(true);
+		
+
+	}
+
+	
 
 }
