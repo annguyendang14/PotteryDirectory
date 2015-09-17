78a445abcce633490595ba6b500462f90a3eb134
taylorv14 <taylorvaughan14@augustana.edu>
2015-09-17 18:55:22 -0500

    Created basic fields and method names. Did not complete the getOrder(), addOrder(), or setOrder() methods. Also set the equals() method to default a false boolean return.

--- FlickerProject/src/Customer.java
+++ FlickerProject/src/Customer.java
@@ -1,9 +1,65 @@
-
+import java.util.*;
 public class Customer {
 	private String name;
+	private String address;
+	private String phoneNum;
+	private String email;	
 
-	public Customer(String customerName) {
-		this.name = customerName;
+	public Customer(String name) {
+		this.name = name;
+	}
+	
+	public Customer(String name, String address){
+		this.name = name;
+		this.address = address;
+	}
+	
+	//public Order[] getOrder(){
+		
+	//}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getAddress(){
+		return address;
+	}
+	
+	public String getPhoneNum(){
+		return phoneNum;
+	}
+	
+	public String getEmail(){
+		return email;
+	}
+	
+	public void addOrder(Order order){
+		
+	}
+	
+	public void setOrder(List<Order> orders){
+		
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setAddress(String address){
+		this.address = address;
+	}
+	
+	public void setEmail(String email){
+		this.email = email;
 	}
 
+	public boolean equals(Object o){
+		return false;
+		
+	}
+	
+	
+	
+	
 }
