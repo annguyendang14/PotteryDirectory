4a223be2df85e80974910aa6b5e2c3f7ccb024fc
annguyendang14 <annguyendang14@augustana.edu>
2015-09-30 16:02:40 -0500

    add (incomplete OrderGUI)

---
+++ FlickerProject/src/NewOrderGUI.java
@@ -0,0 +1,77 @@
+/*
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class NewOrderGUI {
+
+	
+	private JButton save;
+	private JTextField nameText;
+	private JTextField addressText;
+	private JTextField phoneNumText;
+
+	public static void main(String[] args) {
+		NewOrderGUI gui = new NewOrderGUI();
+
+	}
+	public NewOrderGUI() {
+		
+		save = new JButton("Save");
+		nameText = new JTextField("Name: ");
+		addressText = new JTextField("Address: ");
+		phoneNumText = new JTextField("Phone Number: ");
+		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400,300);
+		frame.setTitle("New Order");
+		frame.setLayout(new BorderLayout());
+		JPanel north = new JPanel();
+		north.add(new JLabel("Enter: "));
+		frame.add(north,BorderLayout.NORTH);
+		JPanel center = new JPanel(new GridLayout(3,1));
+		center.add(nameText);
+		center.add(addressText);
+		center.add(phoneNumText);
+		frame.add(center,BorderLayout.CENTER);
+		JPanel south = new JPanel(new FlowLayout());
+		south.add(save);
+		frame.add(south, BorderLayout.SOUTH);
+		frame.setVisible(true);
+		
+
+	}
+
+	
+
+}
+*/
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class NewOrderGUI extends Application{
+	
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("NewOrder.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void main(String[] args){
+		launch(args);
+	}
+	
+}
---
+++ FlickerProject/src/controller/NewOrderController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class NewOrderController {
+
+	@FXML private Label customerName;
+	@FXML private Label dateOrdered;
+	@FXML private Label orderNum;
+	@FXML private TextField customerNameBar;
+	@FXML private DatePicker dateOrderedPicker;
+	@FXML private TextField orderNumBar;
+
+	public void saveOrder() {
+		
+		
+	}
+
+
+
+}
