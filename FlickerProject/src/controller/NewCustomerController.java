e876780a43b090b912cc635e68261a37302f68b9
taylorv14 <taylorvaughan14@augustana.edu>
2015-09-30 22:22:08 -0500

    Added to NewOrderController class. I believe the class would fully work if we can convert the DatePicker dateOrderedPicker into a Date for the Order() constructor.

--- FlickerProject/src/controller/NewCustomerController.java
+++ FlickerProject/src/controller/NewCustomerController.java
@@ -7,6 +7,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import main.*;
 
+/*
+ * 
+ */
 public class NewCustomerController {
 	@FXML private Label title;
 	@FXML private Label name;
@@ -19,7 +22,7 @@ public class NewCustomerController {
 	@FXML private TextField emailBar;
 	@FXML private Button saveOrder;
 	
-	public Customer save(ActionEvent event){
+	public Customer saveCustomer(ActionEvent event){
 		Customer cus = new Customer(nameBar.getText(),addressBar.getText(),phoneNumBar.getText(),emailBar.getText());
 		System.out.println(cus);
 		return cus;
--- FlickerProject/src/controller/NewOrderController.java
+++ FlickerProject/src/controller/NewOrderController.java
@@ -1,11 +1,15 @@
 package controller;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import main.*;
 
 public class NewOrderController {
 
@@ -15,10 +19,13 @@ public class NewOrderController {
 	@FXML private TextField customerNameBar;
 	@FXML private DatePicker dateOrderedPicker;
 	@FXML private TextField orderNumBar;
+	@FXML private Button saveOrder;
 
-	public void saveOrder() {
+	public void saveNewOrder(ActionEvent event) {
+		//INCOMPLETE CONSTRUCTOR CALL. Need to somehow convert DatePicker into Date
 		
-		
+		//Order newOrder = new Order (customerNameBar.getText(), date, Integer.parseInt(orderNumBar.getText()));
+		//System.out.println(newOrder);
 	}
 
 
