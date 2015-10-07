4e8ee8396ba396a37c66570999f5a9e5a748853a
kelseyself14 <kelseyself14@OlinLab101-05.augustana.edu>
2015-10-07 10:46:39 -0500

    small changes

--- FlickerProject/src/data/Customer.java
+++ FlickerProject/src/data/Customer.java
@@ -1,88 +1,90 @@
 package data;
-import java.util.*;
+
+//import java.util.*;
 public class Customer {
 	private String name;
 	private String address;
 	private String phoneNum;
-	private String email;	
+	private String email;
 
-	//why do we have this?-Kelsey
 	public Customer(String name) {
 		this.name = name;
 	}
-	
-	public Customer(String name, String address){
+
+	public Customer(String name, String address) {
 		this(name);
 		this.address = address;
 	}
-	//author-Kelsey created to be used for file processing
-	public Customer(String name, String address, String phoneNum, String email){
+
+	// author-Kelsey created to be used for file processing
+	public Customer(String name, String address, String phoneNum, String email) {
 		this(name, address);
 		this.phoneNum = phoneNum;
 		this.email = email;
 	}
-	//public Order[] getOrder(){
-		
-	//}
-	
-	public String getName(){
-		return name;
-	}
-	
-	public String getAddress(){
-		return address;
-	}
-	
-	public String getPhoneNum(){
-		return phoneNum;
-	}
-	
-	public String getEmail(){
-		return email;
-	}
-	
-	/*public void addOrder(Order order){
-		
-	}
-	
-	public void setOrder(List<Order> orders){
-		
-	}*/
-	
-	public void setName(String name){
-		this.name = name;
-	}
-	
-	public void setAddress(String address){
-		this.address = address;
-	}
-	
-	public void setEmail(String email){
-		this.email = email;
-	}
-	public void setCustomer(String name, String address, String phoneNum, String email){
-		this.setName(name);
-		this.setAddress(address);
-		this.setPhoneNum(phoneNum);
-		this.setEmail(email);
-	}
-	
-	
-	
-	public boolean equals(Object o){
-		return false;
+
+	// to be used if needed
+	// public Order[] getOrder(){
+
+	// }
+
+	public String getName() {
+		return this.name;
 	}
 
-//added for debug purposes
-	public String toString(){
-		return "The Customer: " + this.getName()+ " "+this.getAddress()+ " "+this.getPhoneNum()+ " "+this.getEmail();
+	public String getAddress() {
+		return this.address;
+	}
+
+	public String getPhoneNum() {
+		return this.phoneNum;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	/*
+	 * to be used if needed public void addOrder(Order order){
+	 * 
+	 * }
+	 * 
+	 * public void setOrder(List<Order> orders){
+	 * 
+	 * }
+	 */
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
 	}
 
 	public void setPhoneNum(String phoneNum) {
 		this.phoneNum = phoneNum;
 	}
-	
-	
-	
-	
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setCustomer(String name, String address, String phoneNum, String email) {
+		this.setName(name);
+		this.setAddress(address);
+		this.setPhoneNum(phoneNum);
+		this.setEmail(email);
+	}
+
+	public boolean equals(Object o) {
+		return false;
+	}
+
+	// added for debug purposes, prints out all the data fields to the console
+	public String toString() {
+		return "The Customer: " + this.getName() + " " + this.getAddress() + " "
+				+ this.getPhoneNum() + " " + this.getEmail();
+	}
+
 }
--- FlickerProject/src/data/Order.java
+++ FlickerProject/src/data/Order.java
@@ -1,59 +1,59 @@
 package data;
+
 /**
  * @author annguyendang14
  */
 
 import java.util.Date;
-import java.util.List;
-
 
 public class Order {
 	private Customer customer;
-	//private List<Pottery> potteries;
+	// private List<Pottery> potteries;
 	private String description;
-	private int orderNum; //not sure how to construct this yet
+	private int orderNum; // not sure how to construct this yet
 	private Date orderDate;
 	private Date dueDate;
 	private double price;
-	//private String specialNotice;
-	private int stage; // 0 = not done, 1 = done, 2 = shipped, 3 = customer received
-	
-	
+	// private String specialNotice;
+	private int stage; // 0 = not done, 1 = done, 2 = shipped, 3 = customer
+						// received
+
 	/**
-	 * private constructor to reduce repetition in other constructor
+	 * private constructor to reduce repetition in other constructors
+	 * 
 	 * @param orderDate
 	 * @param orderNum
 	 */
-	private Order(Date orderDate, int orderNum, String description , double price){
+	private Order(Date orderDate, int orderNum, String description, double price) {
 		this.orderDate = orderDate;
 		this.orderNum = orderNum;
 		this.price = price;
 		this.description = description;
 		this.stage = 0;
 	}
-	public Order(String customerName, Date orderDate, int orderNum, String description , double price){
-		this(orderDate, orderNum,description, price);
+
+	public Order(String customerName, Date orderDate, int orderNum, String description, double price) {
+		this(orderDate, orderNum, description, price);
 		this.customer = new Customer(customerName);
-		
-				
+
 	}
-	/*public Order(String customerName, Date orderDate, int orderNum, int stage){
-		this(orderDate, orderNum);
-		this.customer = new Customer(customerName);
-		this.stage = stage;
-				
-	}*/
-	
-				
-	public Order(String customerName, Date orderDate, int orderNum,  String description , double price, int stage){
+
+	/*
+	 * public Order(String customerName, Date orderDate, int orderNum, int
+	 * stage){ this(orderDate, orderNum); this.customer = new
+	 * Customer(customerName); this.stage = stage;
+	 * 
+	 * }
+	 */
+
+	public Order(String customerName, Date orderDate, int orderNum, String description,
+			double price, int stage) {
 		this(customerName, orderDate, orderNum, description, price);
 		this.stage = stage;
-		
-		
+
 	}
-	
-	//added price, stage, and special order by Kelsey **fixed to match new field, fixed for file processing
-	/** 
+
+	/**
 	 * 
 	 * @param orderDate
 	 * @param dueDate
@@ -62,13 +62,15 @@ public class Order {
 	 * @param price
 	 * 
 	 */
-	public Order(String customerName, Date orderDate, Date dueDate, int orderNum,  String description , double price, int stage){
-		this(customerName, orderDate, orderNum,  description , price, stage);
+	public Order(String customerName, Date orderDate, Date dueDate, int orderNum,
+			String description, double price, int stage) {
+		this(customerName, orderDate, orderNum, description, price, stage);
 		this.dueDate = dueDate;
 	}
-	
-	//for file saving
-	public Order(Date orderDate, Date dueDate, int orderNum,  String description , double price, int stage){
+
+	//file saving
+	public Order(Date orderDate, Date dueDate, int orderNum, String description, double price,
+			int stage) {
 		this.orderDate = orderDate;
 		this.orderNum = orderNum;
 		this.dueDate = dueDate;
@@ -76,115 +78,126 @@ public class Order {
 		this.price = price;
 		this.stage = stage;
 	}
-	public Order(Customer customer,Date orderDate, int orderNum, String description , double price){
-		this(orderDate, orderNum,description, price);
+
+	public Order(Customer customer, Date orderDate, int orderNum, String description, double price) {
+		this(orderDate, orderNum, description, price);
 		this.customer = customer;
 	}
-	public Order(Customer customer, Date orderDate, Date dueDate, int orderNum,  String description , double price){
-		this(customer,orderDate, orderNum, description , price);
+
+	public Order(Customer customer, Date orderDate, Date dueDate, int orderNum, String description,
+			double price) {
+		this(customer, orderDate, orderNum, description, price);
 		this.dueDate = dueDate;
 	}
-	
-/*	public int compareTo(Object o){ //need to try later
-		Order other = (Order) o;
-		if(other.orderDate.equals(this.orderDate)){
-			return this.stage-other.stage;
-		} else {
-			return -this.orderDate.compareTo(other.orderDate);
-		}
-	}*/
-	
-/*	public void addPotteries(Pottery pottery){
-		potteries.add(pottery);
-		
-	}
-	
-	public void setPottery(List<Pottery> potteries){
-		this.potteries = potteries;
-	}
-	*/
-	public void setDueDate(Date dueDate){
+
+	/*
+	 * public int compareTo(Object o){ //need to try later Order other = (Order)
+	 * o; if(other.orderDate.equals(this.orderDate)){ return
+	 * this.stage-other.stage; } else { return
+	 * -this.orderDate.compareTo(other.orderDate); } }
+	 */
+
+	/*
+	 * public void addPotteries(Pottery pottery){ potteries.add(pottery);
+	 * 
+	 * }
+	 * 
+	 * public void setPottery(List<Pottery> potteries){ this.potteries =
+	 * potteries; }
+	 */
+	public void setDueDate(Date dueDate) {
 		this.dueDate = dueDate;
 	}
-	
-	public void setPrice(double price){
+
+	public void setPrice(double price) {
 		this.price = price;
 	}
-	
-	/*public void setSpecialNotice(String notice){
-		this.specialNotice = notice;
-	}*/
-	
-	public void setStage(int stage){ //restricted to reduce stage for now, might change later
-		if (stage >= this.stage){
+
+	/*
+	 * public void setSpecialNotice(String notice){ this.specialNotice = notice;
+	 * }
+	 */
+
+	public void setStage(int stage) { // restricted to reduce stage for now,
+										// might change later
+		if (stage >= this.stage) {
 			this.stage = stage;
-		} 
+		}
 	}
-	
-	public Customer getCustomer(){
+
+	public Customer getCustomer() {
 		return customer;
-		
+
 	}
-	
-/*	public List<Pottery> getPotteries(){
-		return potteries;
-		
-	}
-	*/
-	public int getOrderNum(){
+
+	/*
+	 * public List<Pottery> getPotteries(){ return potteries;
+	 * 
+	 * }
+	 */
+	public int getOrderNum() {
 		return orderNum;
-		
+
 	}
-	
-	public Date getOrderDate(){
+
+	public Date getOrderDate() {
 		return orderDate;
-		
+
 	}
-	
-	public Date getDueDate(){
+
+	public Date getDueDate() {
 		return dueDate;
-		
+
 	}
-	
-	public double getPrice(){
+
+	public double getPrice() {
 		return price;
-		
+
 	}
-	
-/*	public String getNotice(){
-		return specialNotice;
-		
-	}*/
-	
-	public int getStage(){
+
+	/*
+	 * public String getNotice(){ return specialNotice;
+	 * 
+	 * }
+	 */
+
+	public int getStage() {
 		return stage;
-		
+
 	}
+
 	/**
 	 * temp toString method, need revision later
 	 */
-	public String toString(){
-		if (dueDate != null){
-			return orderNum+" "+ orderDate.toString() +" "+dueDate.toString()+" "+stage+" "+description;
+	public String toString() {
+		if (dueDate != null) {
+			return orderNum + " " + orderDate.toString() + " " + dueDate.toString() + " " + stage
+					+ " " + description;
 		} else {
-			return orderNum+" "+ orderDate.toString() +" "+stage+" "+description;
+			return orderNum + " " + orderDate.toString() + " " + stage + " " + description;
 
 		}
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public void setOrder(Customer customer,Date orderDate, String description , double price, int stage){
+
+	public void setOrder(Customer customer, Date orderDate, String description, double price,
+			int stage) {
 		this.setCustomer(customer);
 		this.setOrderDate(orderDate);
 		this.setDescription(description);
 		this.setPrice(price);
 		this.setStage(stage);
 	}
-	public void setOrder(Customer customer,Date orderDate, Date dueDate , String description , double price, int stage){
+
+	public void setOrder(Customer customer, Date orderDate, Date dueDate, String description,
+			double price, int stage) {
 		this.setOrder(customer, orderDate, description, price, stage);
 		this.setDueDate(dueDate);
 	}
@@ -192,9 +205,9 @@ public class Order {
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
+
 	public void setOrderDate(Date orderDate) {
 		this.orderDate = orderDate;
 	}
-	
 
 }
